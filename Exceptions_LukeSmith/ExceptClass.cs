ee812e0abc55c0e140f49a54eb2116c090eb53c3
Luke-Smith-5113 <114005746+Luke-Smith-5113@users.noreply.github.com>
2023-09-06 08:43:11 -0500

    Initial commit

---
+++ Exceptions_LukeSmith/ExceptClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions_LukeSmith
+{
+    internal class ExceptClass
+    {
+        public float Divide(float x,  float y)
+        {
+            if (y == 0) { throw new DivideByZeroException(); }
+            else { return x / y; }
+        }
+        public void CheckAge(int age)
+        {
+            if (age >= 17) { Console.WriteLine($"You are {age}, you can play mature games"); }
+            else { throw new ArgumentException(); }
+        }
+    }
+}
---
+++ Exceptions_LukeSmith/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exceptions_LukeSmith;
+
+float f1 = 0.0f;
+float f2 = 1.0f;
+float result = 0;
+Random rnd = new Random();
+int ranInt = rnd.Next(2, 30);
+ExceptClass e = new ExceptClass();
+try
+{ result = e.Divide(f2, f1); }
+catch { Exception exception = new Exception(); Console.WriteLine(exception.Message); Console.WriteLine("Try inputing a non-zero floating point number");
+    try { float userVal = float.Parse(Console.ReadLine()); result = e.Divide(f2, userVal); Console.WriteLine(result); }
+    catch { Exception exception2 = new Exception(); Console.WriteLine(exception2.Message); }
+}
+finally { Console.WriteLine($"Calculations completed with a result of {result}"); }
+try { e.CheckAge(ranInt); }
+catch { Exception exception = new Exception(); Console.WriteLine(exception.Message); }
